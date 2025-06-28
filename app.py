5172e6fd751d35a7e4351e7d90d361f40a3f0040
Narayane Demetrio <amocarnecrua@gmail.com>
2025-06-28 20:29:00 -0300

    initial hangman logic

--- app.py
+++ app.py
@@ -7,6 +7,23 @@ def select_word():
     word = random.choice(words)
     return word.upper()
 
+def play_hangman(word):
+    word_completion = "_ " * len(word)  # create a string of underscores
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    attempts = 6  # number defined on the scope
+
+# welcoming message
+    print("Welcome to Hangman!")
+    print(show_status(attempts))
+    print(f"This is the word: {word_completion}")
+
+# while user has not guessed the word and attempts are left
+    while not guessed and attempts > 0:
+        guess = input("Please guess a word or a letter: ").upper()
+        print(guess)
+
 # check game status
 def show_status(attempts):
     stages = [
@@ -70,3 +87,9 @@ def show_status(attempts):
 
     return stages[attempts]
 
+#start the game
+def start_game():
+    word = select_word()
+    play_hangman(word)
+
+start_game()
